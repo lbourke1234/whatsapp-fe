6484713b655a8c7a47c9ae19fe1c62fe74618d05
lbourke1234 <lbourke1234@gmail.com>
2022-07-12 11:55:42 +0100

    Design update

--- src/components/RightHeading.jsx
+++ src/components/RightHeading.jsx
@@ -1,5 +1,4 @@
 import { Row, Col } from 'react-bootstrap'
-import { AiOutlineLeftCircle } from 'react-icons/ai'
 import { AiOutlineMinus } from 'react-icons/ai'
 import { AiOutlineEllipsis } from 'react-icons/ai'
 import { AiOutlineSearch } from 'react-icons/ai'
@@ -8,16 +7,22 @@ const RightHeading = () => {
   return (
     <>
       <Col md={2} className="d-flex align-items-center pl-5 top-right-heading-icons">
-        <AiOutlineLeftCircle />
+        <img
+          className="heading-profile-image"
+          src="http://placekitten.com/200/300"
+          alt="kitten"
+        ></img>
       </Col>
       <Col md={8}>
         <Row>
           <Col>
-            <span>Conversation Name Here</span>
+            <span className="conversation-name">Conversation Name Here</span>
           </Col>
         </Row>
         <Row>
-          <span>Map through people in chat</span>
+          <Col>
+            <span className="chat-preview">Map through people in chat</span>
+          </Col>
         </Row>
       </Col>
       <Col className="d-flex justify-content-center align-items-center">
--- src/components/SingleConversation.jsx
+++ src/components/SingleConversation.jsx
@@ -1,24 +1,28 @@
 import { Col, Row } from 'react-bootstrap'
-import { AiOutlineLeftCircle } from 'react-icons/ai'
 
 const SingleConversation = () => {
   return (
     <>
       <Col md={2} className="d-flex justify-content-center align-items-center">
-        <AiOutlineLeftCircle className="ai-outline-left-circle" />
+        <img
+          className="profile-image"
+          src="http://placekitten.com/200/300"
+          alt="kitten"
+        ></img>
       </Col>
       <Col md={10}>
         <Row>
           <Col md={10}>
-            <span>Conversation Name Here</span>
+            <span className="conversation-name">Conversation Name Here</span>
           </Col>
           <Col md={2}>
             <span className="day">Yesterday</span>
           </Col>
         </Row>
         <Row>
-          <span className="pl-3">Preview of chat goes here</span>
+          <span className="pl-3 chat-preview">Preview of chat goes here</span>
         </Row>
+        <hr></hr>
       </Col>
     </>
   )
