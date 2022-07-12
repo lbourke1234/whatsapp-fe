a608af733b9ab425ab2305439ee9f03aa4159b44
lbourke1234 <lbourke1234@gmail.com>
2022-07-12 09:42:17 +0100

    update to RightChat.jsx

--- src/components/RightChat.jsx
+++ src/components/RightChat.jsx
@@ -1,18 +1,38 @@
 import SingleChat from './SingleChat'
+import { useEffect } from 'react'
 
-const RightChat = () => (
-  <>
-    <SingleChat />
-    <SingleChat />
-    <SingleChat />
-    <SingleChat />
-    <SingleChat />
-    <SingleChat />
-    <SingleChat />
-    <SingleChat />
-    <SingleChat />
-    <SingleChat />
-  </>
-)
+const RightChat = () => {
+  const token = window.localStorage.getItem('token')
+
+  useEffect(() => {
+    fetchMessages()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const fetchMessages = async () => {
+    const response = await fetch('http://localhost:5001/messages', {
+      headers: {
+        Authentication: `Bearer ${token}`
+      }
+    })
+    const body = await response.json()
+    console.log(body)
+  }
+
+  return (
+    <>
+      <SingleChat />
+      <SingleChat />
+      <SingleChat />
+      <SingleChat />
+      <SingleChat />
+      <SingleChat />
+      <SingleChat />
+      <SingleChat />
+      <SingleChat />
+      <SingleChat />
+    </>
+  )
+}
 
 export default RightChat
