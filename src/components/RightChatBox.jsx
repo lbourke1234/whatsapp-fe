1d35880a7448b3792b540cd2f2c8f6e4bfb8017f
lbourke1234 <lbourke1234@gmail.com>
2022-07-13 11:49:48 +0100

    right chat box push

--- src/components/RightChatBox.jsx
+++ src/components/RightChatBox.jsx
@@ -2,8 +2,13 @@ import { Col, Form } from 'react-bootstrap'
 import { AiOutlineSmile } from 'react-icons/ai'
 import { AiOutlinePaperClip } from 'react-icons/ai'
 import { AiFillSound } from 'react-icons/ai'
+import { setState } from 'react'
 
 const RightChatBox = () => {
+  const [message, setMessage] = setState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   return (
     <>
       <Col md={1} className="d-flex justify-content-between align-items-center">
@@ -13,7 +18,11 @@ const RightChatBox = () => {
       <Col md={10} className="pt-3">
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Type a message" />
+            <Form.Control
+              type="text"
+              placeholder="Type a message"
+              onChange={(e) => setMessage(e.target.value)}
+            />
           </Form.Group>
         </Form>
       </Col>
