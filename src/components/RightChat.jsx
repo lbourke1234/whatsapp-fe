da4dbff806cedf85d9b2ebfafe0a2a37d25d5f8f
lbourke1234 <lbourke1234@gmail.com>
2022-07-13 09:52:03 +0100

    update right side chat

--- src/components/RightChat.jsx
+++ src/components/RightChat.jsx
@@ -1,20 +1,9 @@
 import SingleChat from './SingleChat'
 import { useSelector } from 'react-redux'
-import { useEffect } from 'react'
 
 const RightChat = () => {
-  // const userInfo = useSelector((state) => state)
   const allChats = useSelector((state) => state.user.history.history)
-
-  // useEffect(() => {
-  //   console.log(allChats)
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [])
-
-  // useEffect(() => {
-  //   console.log(allChats)
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [allChats])
+  console.log('ALL CHATS', allChats)
 
   return (
     <>
--- src/components/SingleChat.jsx
+++ src/components/SingleChat.jsx
@@ -1,12 +1,13 @@
 import { Row, Col } from 'react-bootstrap'
 
-const SingleChat = (chat) => {
+const SingleChat = ({ chat }) => {
   return (
     <Col md={12} className="my-2 px-5 single-chat-container">
-      <Row>Name of Sender</Row>
+      {console.log(chat)}
+      <Row>{chat.sender}</Row>
       <Row>
-        <span className="conversation-text">hello</span>
-        <span className="ml-auto pr-3">time</span>
+        <span className="conversation-text">{chat.content.text}</span>
+        <span className="ml-auto pr-3">{chat.updatedAt}</span>
       </Row>
     </Col>
   )
