10f81013cc39469994abd406f89e495c136ee67a
lbourke1234 <lbourke1234@gmail.com>
2022-07-15 15:37:47 +0100

    fe push

--- src/components/SingleConversation.jsx
+++ src/components/SingleConversation.jsx
@@ -1,32 +1,28 @@
-import { Col, Row } from "react-bootstrap";
-import { useDispatch } from "react-redux";
-import { setChatIdAction, setCurrentRoomAction } from "../redux/actions";
+import { Col, Row } from 'react-bootstrap'
+import { useDispatch } from 'react-redux'
+import { setChatIdAction, setCurrentRoomAction } from '../redux/actions'
 
 const SingleConversation = (chat) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   return (
     <div onClick={() => dispatch(setCurrentRoomAction(chat))}>
-      {console.log("chat", chat)}
+      {console.log('chat', chat)}
       <Col md={2} className="d-flex justify-content-center align-items-center">
-        {/* <img className="profile-image" src={chat.content.media} alt="kitten"></img> */}
+        {/* <img className="profile-image" src={chat.chatcontent.media} alt="kitten"></img> */}
       </Col>
       <Col md={10}>
         <Row>
           <Col md={10}>
-            {/* <span className="conversation-name">{chat.sender}</span> */}
+            <span className="conversation-name">{chat.chat._id}</span>
           </Col>
-          <Col md={2}>
-            {/* <span className="day">{chat.createdAt}</span> */}
-          </Col>
+          <Col md={2}>{/* <span className="day">{chat.createdAt}</span> */}</Col>
         </Row>
-        <Row>
-          {/* <span className="pl-3 chat-preview">{chat.content.text}</span> */}
-        </Row>
+        <Row>{/* <span className="pl-3 chat-preview">{chat.content.text}</span> */}</Row>
         <hr></hr>
       </Col>
       hello
     </div>
-  );
-};
-export default SingleConversation;
+  )
+}
+export default SingleConversation
